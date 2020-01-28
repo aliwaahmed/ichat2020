31e5a7c8246406b1db8d108e1870fbb1ac484a11
aliwaahmed <aliwaahmed/iChat>
2020-01-28 23:24:42 +0200

    28

--- app/src/main/java/com/chatapp/abobakrdev/egychat2/MainActivity.java
+++ app/src/main/java/com/chatapp/abobakrdev/egychat2/MainActivity.java
@@ -36,6 +36,13 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -49,13 +56,18 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     private GoogleApiClient googleApiClient;
     TextView textView;
     private static final int RC_SIGN_IN = 1;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(getApplicationContext());
+
+        mAuth = FirebaseAuth.getInstance();
 
         GoogleSignInOptions gso =  new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
+                .requestIdToken("120873551649-8283s03tmnp17nf7mh8s7sm7uajf4ove.apps.googleusercontent.com")
                 .build();
         googleApiClient=new GoogleApiClient.Builder(this)
                 .enableAutoManage(MainActivity.this,this)
@@ -74,8 +86,33 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         });
 
 
+
+
+
     }
 
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+        Log.d("TAG", "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithCredential:failure", task.getException());
+                        }
+
+
+                    }
+                });
+    }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
@@ -94,6 +131,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
         Log.e("result", String.valueOf(result.getStatus()));
         if(result.isSuccess()){
             gotoProfile();
+            firebaseAuthWithGoogle(result.getSignInAccount());
         }else{
             Toast.makeText(getApplicationContext(),"Sign in cancel",Toast.LENGTH_LONG).show();
         }
