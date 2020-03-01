76396749cc5dce1dd780cb64061719d7e499eba9
aliwaahmed <aliwaahmed/iChat>
2020-03-01 13:33:14 +0200

    26

---
+++ app/src/main/java/com/chatapp/abobakrdev/egychat2/ActiveUser/ui/Setting/Viewimage.java
@@ -0,0 +1,31 @@
+package com.chatapp.abobakrdev.egychat2.ActiveUser.ui.Setting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.chatapp.abobakrdev.egychat2.R;
+
+public class Viewimage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scene_view_img);
+
+        Glide.with(this).load(getIntent().getExtras().getString("path"))
+                .into((ImageView) findViewById(R.id.imageView4));
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        overridePendingTransition( R.anim.slide_in_up, R.anim.slide_out_up );
+
+        super.onBackPressed();
+
+    }
+}
