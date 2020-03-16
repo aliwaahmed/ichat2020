3901204cec6bc91ae755eb17187ae0b3cf62c616
logapps <log.apps2020@gmail.com>
2020-03-16 07:44:51 -0700

    16

--- app/src/main/java/com/chatapp/abobakrdev/egychat2/ActiveUser/Chat/ChatAdapter/Adapter.java
+++ app/src/main/java/com/chatapp/abobakrdev/egychat2/ActiveUser/Chat/ChatAdapter/Adapter.java
@@ -1,5 +1,41 @@
 package com.chatapp.abobakrdev.egychat2.ActiveUser.Chat.ChatAdapter;
 
-public class Adapter {
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Adapter extends RecyclerView.Adapter<Viewholder>  {
+
+
+
+    @Override
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
+    }
+
+    @NonNull
+    @Override
+    public Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Viewholder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
+
+class  Viewholder extends RecyclerView.ViewHolder
+{
+
+    public Viewholder(@NonNull View itemView) {
+        super(itemView);
+    }
 }
