1745e1c2392bb11c23a5c2a441328e465b62fa55
aliwaahmed <aliwaahmed/iChat>
2020-04-06 15:18:40 +0200

    28

--- app/src/main/java/com/chatapp/abobakrdev/egychat2/FCM/MyFirebaseMessagingService.java
+++ app/src/main/java/com/chatapp/abobakrdev/egychat2/FCM/MyFirebaseMessagingService.java
@@ -116,7 +116,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         Uri defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
         Intent intent = new Intent(getApplicationContext(), Chat_Activity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP|Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.putExtra("mail", mail);
         intent.putExtra("name", name);
         intent.putExtra("img", img);
